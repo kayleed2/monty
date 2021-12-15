12552d0ec9606bed29e41c46fbe01526d26af4bb
Kaylee Dao <3597@holbertonschool.com>
2021-12-15 13:47:59 -0800

    Documentation

--- opcode_funcs.c
+++ opcode_funcs.c
@@ -1,4 +1,23 @@
 #include "monty.h"
 /**
  * op_pall - Prints all values of stack
- * 
+ * @head: Stack list
+ * @i: Line number
+ *
+ * Return: Void
+ */
+
+void op_pall(stack_t head, unsigned int i)
+{
+}
+
+/**
+ * op_pall - Prints all values of values
+ * @head: Stack list
+ * @i: Line number
+ * Return: Void
+ */
+
+void op_pall(stack_t head, unsigned int i)
+{
+}
