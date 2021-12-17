d837c975b0929c9e49c794ab347f1b3e0f83b588
Kaylee Dao <3597@holbertonschool.com>
2021-12-17 06:16:47 -0800

    Adding splitter file

---
+++ splitter.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * splitter - Splits string into tokens
+ * @str: String to split
+ * Return: Pointer to array of strings
+ */
+
+char **splitter(char *str)
+{
+	int len = strlen(str);
+	int i;
+	char *string = NULL;
+	char **holder = NULL;
+
+	holder = malloc(sizeof(char *) * (len * 2));
+	string = strtok(str, " ");
+	for (i = 0; string != NULL; i++)
+	{
+		holder[i] = string;
+		string = strtok(NULL, " ");
+	}
+	holder[i] = '\0';
+	return (holder);
+}
